2a461f196efd56113b16115f407b998859547579
jorgeobnd <ryuuzaki.dev@gmail.com>
2026-03-12 10:42:54 -0600

    WIP

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  standalone: false,
+  template: '<app-inventory></app-inventory>',
+  // styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'frontend';
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+import { AppComponent } from './app.component';
+import { InventoryComponent } from './components/inventory/inventory.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InventoryComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    CommonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
